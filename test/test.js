3e024dd31ab3a853f13a4f8716f12b797bef678f
Rajesh <rajesh.kathiriya507@gmail.com>
2015-09-25 11:34:38 +0530

    changed test.js

--- test/test.js
+++ test/test.js
@@ -6,6 +6,6 @@ var randomQoutesGenerator = require('../');
 describe('random-qoutes-generator node module', function () {
   it('must have at least one test', function () {
     randomQoutesGenerator();
-   // assert(false, 'I was too lazy to write any tests. Shame on me.');
+    assert(true, 'I was too lazy to write any tests. Shame on me.');
   });
 });
