c94b1aff0cb13f6df62c2bac591ed3cf6faf636f
Rajesh <rajesh.kathiriya507@gmail.com>
2015-09-25 11:02:18 +0530

    changed index.html for unsued variable

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 'use strict';
-module.exports = function (str) {
+module.exports = function () {
   var quotes_json = require('./quotes.json');
   var quote=quotes_json[Math.floor(Math.random()*quotes_json.length)];
   return quote;
